0ef4317d7316b96244a5183a9ab6a92d464d9f62
WvlRampage <ramapage23@gmail.com>
2020-03-11 11:12:37 -0500

    added component footer

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./../../components/Statics/navBar";
 import Sliders from "./../../components/Slider";
+import Footer from "./../../components/Footer";
 
 const Home = () => {
   return (
     <section>
       <NavBar />
       <Sliders />
+      <Footer />
     </section>
   );
 };
